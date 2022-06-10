f045dc96deec70791276a994b191d4f5417c0606
guowh <973886860@qq.com>
2022-06-10 16:47:07 +0800

    1. 接入swagger文档; 2. 更新依赖

--- demo/rest-api/src/main.ts
+++ demo/rest-api/src/main.ts
@@ -2,11 +2,26 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { ValidationPipe } from './pipe/validation.pipe';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);  // 创建应用程序实例，此时所有被 AppModule 导入的其他模块的所有实例都会被加载
+
     app.setGlobalPrefix('api'); // 全局前缀路由
-    app.useGlobalPipes(new ValidationPipe());
+
+    app.useGlobalPipes(new ValidationPipe()); // 全局配置管道
+
+    // 配置 Swagger
+    const options = new DocumentBuilder()
+        .setTitle('Nest zero to one')
+        .setDescription('The nest-zero-to-one API description')
+        .setVersion('1.0')
+        .addTag('test')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api-doc', app, document);
+
+
     await app.listen(3000);  // 使用3000端口监听应用程序
 }
 
--- demo/rest-api/src/modules/user/user.dto.ts
+++ demo/rest-api/src/modules/user/user.dto.ts
@@ -1,40 +1,58 @@
 import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 
 export class GetCodeDTO {
+
+    @ApiProperty()
     @IsEmail()
     @IsNotEmpty()
     readonly email: string;
 }
 
 export class GetEmailCodeDTO {
+
+    @ApiProperty()
     @IsEmail()
     @IsNotEmpty()
     readonly email: string;
+
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     readonly code: string;
 }
 
 export class LoginDTO {
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     readonly nickname: string;
+
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     readonly password: string;
 }
 
 export class RegisterDTO {
+
+    @ApiProperty()
     @IsEmail()
     @IsNotEmpty()
     readonly email: string;
+
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     readonly nickname: string;
+
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     readonly password: string;
+
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     readonly code: string;
